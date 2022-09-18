7bdf377f7198992d95485f1a20e458db16bc1f27
Wachira salome <101164161+Sali44siwa@users.noreply.github.com>
2022-09-19 01:43:52 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+//change navbar styles
+
+window.addEventListener('scroll', () => {
+        document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 100)
+    })
+    //show or hide faqs
+const faqs = document.querySelectorAll('.faq');
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        //change icon 
+        const icon = faq.querySelector('.faq_icon i');
+        if (icon.className === 'uil uil-plus') { icon.className = 'uil uil-minus' } else { icon.className = 'uil uil-plus' }
+
+    })
+})
